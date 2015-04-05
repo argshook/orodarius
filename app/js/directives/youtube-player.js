e5924448b223931f3b3a7c7554c4c939158f600b
arijus <arijuss@wix.com>
2015-04-06 02:16:23 +0300

    refactored youtube-player directive a bit

--- app/js/directives/youtube-player.js
+++ app/js/directives/youtube-player.js
@@ -2,13 +2,11 @@
   'use strict';
 
   angular.module('orodarius')
-    .directive('youtubePlayer', function($window) {
+    .directive('youtubePlayer', function($window, PlayerService) {
       return {
         restrict: 'A',
         replace: false,
         link: function(scope, element, attrs) {
-          var player;
-
           var tag = $('<script>').attr({
             src: 'https://www.youtube.com/iframe_api',
             async: true
@@ -20,44 +18,7 @@
           $('script').eq(0).before(tag);
 
           $window.onYouTubeIframeAPIReady = function() {
-            // TODO: move player to service
-            player = new YT.Player('main-video-player', {
-              width: $window.innerWidth,
-              height: $window.innerHeight,
-              videoId: 'DT2oAtQtFrg',
-              playerVars: {
-                autohide: 1,
-                autoplay: 1,
-                // controls: 0,
-                disablekb: 0,
-                enablejsapi: 1
-              },
-              events: {
-                onReady: function() {
-                  console.log('onReady');
-                  player.playVideo();
-                },
-                onStateChange: function(event) {
-                  // event.data holds event type, one of the following:
-                  // -1 (unstarted)
-                  // 0 (ended)
-                  // 1 (playing)
-                  // 2 (paused)
-                  // 3 (buffering)
-                  // 5 (video cued).
-
-                  // if event is 0 (ended), play another video.
-                  if (event.data ===  0) {
-                    player.loadVideoById({
-                      videoId: '1DC2xc_Zul8',
-                      startSeconds: 0,
-                      // endSeconds,
-                      suggestedQuality: 'large'
-                    });
-                  }
-                }
-              }
-            });
+            PlayerService.newPlayer(attrs.youtubePlayer || 'main-video-player');
           };
         }
       };
--- test/unit/directives/youtube-player.spec.js
+++ test/unit/directives/youtube-player.spec.js
@@ -1,24 +1,41 @@
 'use strict';
 
 describe('Directive: youtube-player', function() {
-  var element, scope, $window;
+  var element, scope, $compile,
+      $window, PlayerService;
 
   beforeEach(module('orodarius'));
 
-  beforeEach(inject(function(_$compile_, _$rootScope_, _$window_) {
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$window_, _PlayerService_) {
     scope = _$rootScope_.$new();
-    element = _$compile_('<div youtube-player></div>')(scope);
+    $compile = _$compile_;
     $window = _$window_;
+    PlayerService = _PlayerService_;
 
-    spyOn(YT, 'Player'); // YT comes from test/mocks/YT.js
-    $window.onYouTubeIframeAPIReady();
+    spyOn(PlayerService, 'newPlayer');
+
   }));
 
+  function compileWith(id) {
+    element = $compile('<div youtube-player="'+ (id || 'main-video-player') +'"></div>')(scope);
+
+    // the following is called when iframe_api loads, so we call
+    // this manually to test
+    $window.onYouTubeIframeAPIReady();
+  }
+
   it('should compile successfully', function() {
+    compileWith();
     expect(element.find('[youtube-player]')).toBeTruthy();
   });
 
-  it('should call onYouTubeIframeAPIReady once created', function() {
-    expect(YT.Player).toHaveBeenCalled();
+  it('should call PlayerService.newPlayer with default selector ID, once directive is compiled', function() {
+    compileWith();
+    expect(PlayerService.newPlayer).toHaveBeenCalledWith('main-video-player');
+  });
+
+  it('should call PlayerService.newPlayer with id selector, if its passed to youtube-player directive', function() {
+    compileWith('tinky-winky');
+    expect(PlayerService.newPlayer).toHaveBeenCalledWith('tinky-winky');
   });
 });
