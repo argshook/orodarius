11f7124d4299627db1599db61a2167b15f167cb3
arijus <arijuss@wix.com>
2015-04-10 00:36:26 +0300

    updated iframe size to correspond to viewport

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,11 @@
 ;(function() {
   'use strict';
 
-  angular.module('orodarius', []);
+  angular.module('orodarius', []).run(function($window) {
+    $($window).on('resize', _.throttle(function() {
+      $('#main-video-player')
+        .attr('width', $window.innerWidth)
+        .attr('height', $window.innerHeight);
+    }, 400));
+  });
 })();
