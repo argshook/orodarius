0566f0788ca0095b8f22b8c6c420877fd76f2163
arijus <arijuss@wix.com>
2015-04-15 22:32:52 +0300

    changed deploy.sh permissions

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,10 @@
 mkdir ../orodarius-public
 cp -r public/* ../orodarius-public/
 git checkout gh-pages
-rm -r *
 cp -r ../orodarius-public/* .
-git add .
-git commit -m "updated release"
-git push origin gh-pages
-git checkout master
-echo "Done!"
-rm -rf ../orodarius-public
+# git add .
+# git commit -m "updated release"
+# git push origin gh-pages
+# git checkout master
+# echo "Done!"
+# rm -rf ../orodarius-public
