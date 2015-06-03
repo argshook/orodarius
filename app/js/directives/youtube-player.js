8c32ceecb98d0ea67555520cd17dfa4071465300
arijus <arijuss@wix.com>
2015-06-04 00:02:50 +0300

    updated youtube-player tests to not load youtube api for each test

--- app/js/directives/youtube-player.js
+++ app/js/directives/youtube-player.js
@@ -7,15 +7,20 @@
         restrict: 'A',
         replace: false,
         link: function(scope, element, attrs) {
-          var tag = $('<script>').attr({
-            src: 'https://www.youtube.com/iframe_api',
-            async: true
-          });
+          // scope.isDisabled comes from karma tests.
+          // TODO: nothing like that should be done, figure out how to
+          // prevent script src loading during karma tests
+          if(!scope.isDisabled) {
+            var tag = $('<script>').attr({
+              src: 'https://www.youtube.com/iframe_api',
+              async: true
+            });
 
-          // this is suggested here:
-          // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
-          // not sure if it really needs to be first
-          $('script').eq(0).before(tag);
+            // this is suggested here:
+            // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+            // not sure if it really needs to be first
+            $('script').eq(0).before(tag);
+          }
 
           $window.onYouTubeIframeAPIReady = function() {
             PlayerService.createNewPlayer(attrs.youtubePlayer || 'main-video-player');
