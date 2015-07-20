663fed666002c517f8c105124dec3e2ee42fc28f
arijus <arijuss@wix.com>
2015-07-20 23:57:30 +0300

    increased baseCtrl coverage

--- test/unit/controllers/baseCtrl.spec.js
+++ test/unit/controllers/baseCtrl.spec.js
@@ -1,12 +1,14 @@
 'use strict';
 
 describe('Controller: baseCtrl', function() {
-  var ctrl, scope;
+  var ctrl, scope, PlayerService, SidebarService;
 
   beforeEach(module('orodarius'));
-  beforeEach(inject(function(_$controller_, _$rootScope_) {
+  beforeEach(inject(function(_$controller_, _$rootScope_, _PlayerService_, _SidebarService_) {
     scope = _$rootScope_.$new();
     ctrl = _$controller_('baseCtrl', { $scope: scope });
+    PlayerService = _PlayerService_;
+    SidebarService = _SidebarService_;
   }));
 
   it('should contain keyboardEventsOptions object', function() {
@@ -16,5 +18,39 @@ describe('Controller: baseCtrl', function() {
   it('keyboardEventsOptions should contain at least left/right key functions', function() {
     expect(ctrl.keyboardEventsOptions[37]).toBeDefined();
     expect(ctrl.keyboardEventsOptions[39]).toBeDefined();
+  });
+
+  describe('keyboardEventsOptions', function() {
+    beforeEach(function() {
+      spyOn(scope, '$apply').and.callThrough();
+    });
+
+    it('should call PlayerService.playPrevious on left key', function() {
+      spyOn(PlayerService, 'playPrevious');
+      ctrl.keyboardEventsOptions[37]();
+
+      expect(PlayerService.playPrevious).toHaveBeenCalled();
+      expect(scope.$apply).toHaveBeenCalled();
+    });
+
+    it('should call PlayerService.playNext on right key', function() {
+      spyOn(PlayerService, 'playNext');
+      ctrl.keyboardEventsOptions[39]();
+
+      expect(PlayerService.playNext).toHaveBeenCalled();
+      expect(scope.$apply).toHaveBeenCalled();
+    });
+
+    it('should call PlayerService.playOrPause on space key', function() {
+      spyOn(PlayerService, 'playOrPause');
+      ctrl.keyboardEventsOptions[32]();
+      expect(PlayerService.playOrPause).toHaveBeenCalled();
+    });
+
+    it('should call SidebarService.toggle on space key', function() {
+      spyOn(SidebarService, 'toggle');
+      ctrl.keyboardEventsOptions[16]();
+      expect(SidebarService.toggle).toHaveBeenCalled();
+    });
   });
 });
