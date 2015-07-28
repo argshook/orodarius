7be85f0121ea212a83812b6f4a368767f4b17de2
arijus <arijuss@wix.com>
2015-07-28 21:30:22 +0300

    increased test coverage

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,3 +1,21 @@
+'use strict';
+
 describe("Module: orodarius", function() {
-  beforeEach(module('orodarius'));
+  var $window, $rootScope;
+
+  beforeEach(inject(function(_$window_, _$rootScope_) {
+    $window = _$window_;
+    $rootScope = _$rootScope_;
+  }));
+
+  describe('run block', function() {
+    var orodariusModule = angular.module('orodarius'),
+        runBlock = orodariusModule._runBlocks[0];
+
+    it('should attach resize event event', function() {
+      spyOn($.fn, 'on').and.callThrough();
+      runBlock();
+      expect($.fn.on).toHaveBeenCalledWith('resize', jasmine.any(Function));
+    });
+  });
 });
