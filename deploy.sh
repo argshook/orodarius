58d6be8e0a71a4a3eadeeda61aa82d25324db90d
arijus <arijuss@wix.com>
2015-10-11 22:40:05 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
+echo "Running dev build"
+brunch b || { echo "dev build failed :(" ; exit 1; }
+
+echo "Running tests"
+npm run test-single || { echo "test failed :(" ; exit 1; }
+
+echo "Running prod build"
+brunch b -P || { echo "build failed :("; exit 1; }
+
+echo "Creating temp directory"
 mkdir ../orodarius-public
+
+echo "Moving built files to temp directory"
 cp -r public/* ../orodarius-public/
+
+echo "Cheking out gh-pages"
 git checkout gh-pages
+
+echo "Moving built files from temp directory"
 cp -r ../orodarius-public/* .
-# git add .
-# git commit -m "updated release"
-# git push origin gh-pages
-# git checkout master
-# echo "Done!"
-# rm -rf ../orodarius-public
+
+echo "Committing updates"
+git add .
+git commit -m "updated release"
+
+echo "Pushing to origin"
+git push origin gh-pages
+git checkout master
+
+echo "Removing temp directory"
+rm -rf ../orodarius-public
+
+echo "Done!"
