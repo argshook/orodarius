c12a6c2a9ceeb4204370af440a125f703c7cfbb4
argshook <argshook@gmail.com>
2015-11-01 18:13:43 +0200

    remove old playerOverlay ctrl
    now moved to directive

--- app/js/controllers/playerOverlayCtrl.js
+++
@@ -1,8 +0,0 @@
-;(function() {
-  'use strict';
-
-  angular.module('orodarius')
-    .controller('playerOverlayCtrl', function($scope, PlayerService, PlaylistService) {
-      $scope.playerService = PlayerService;
-    });
-})();
