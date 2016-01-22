9fab5f28dfc45ab60a097060bbe19a3a22da8eb1
argshook <argshook@gmail.com>
2016-01-23 01:31:33 +0200

    fix ng-enter tests

--- test/unit/directives/ng-enter.spec.js
+++ test/unit/directives/ng-enter.spec.js
@@ -1,42 +1,42 @@
 'use strict';
 
 describe('Directive: ngEnter', function() {
-  var element, scope, $compile;
+  var compile;
+
+  var parentScopeMock = {
+    calback: _.noop,
+    options: {}
+  };
+
+  var elementAttributesMock = {
+    'ng-enter': 'callback',
+    'ng-enter-options': 'options'
+  };
 
   beforeEach(module('orodarius'));
-  beforeEach(inject(function(_$rootScope_, _$compile_) {
-    scope = _$rootScope_.$new();
-    $compile = _$compile_;
-
-    scope.callback = angular.noop;
+  beforeEach(inject(function($rootScope, $compile) {
+    compile = createCompiler('<input type="text" ng-enter="callback" ng-enter-options="options" />', $rootScope, $compile);
   }));
 
-  function compileWith(callback, options) {
-    options = options || {};
-    var template = _.template('<input type="text" ng-enter="${callback}()" ng-enter-options="${options}" />', {
-      callback: callback,
-      options: JSON.stringify(options).replace(/"/g, "'")
-    });
-    element = $compile(template)(scope);
-    scope.$digest();
-  }
-
   it('should invoke callback when enter key is pressed', function() {
-    spyOn(scope, 'callback');
-    compileWith('callback');
-    var event = $.Event('keypress');
-    event.which = 13;
-    event.keyCode = 13;
-    element.triggerHandler(event);
-    expect(scope.callback).toHaveBeenCalled();
+    compile(parentScopeMock, function(scope, element) {
+      spyOn(scope, 'ngEnter');
+      let event = $.Event('keypress');
+      event.which = 13;
+      event.keyCode = 13;
+      element.triggerHandler(event);
+      expect(scope.ngEnter).toHaveBeenCalled();
+    });
   });
 
   it('should blur element if ng-enter-options have blurOnEnter set to true', function() {
-    compileWith('callback', { blurOnEnter: true });
-    spyOn(element[0], 'blur');
-    var event = $.Event('keypress');
-    event.which = 13;
-    element.triggerHandler(event);
-    expect(element[0].blur).toHaveBeenCalled();
+    compile(_.assign(parentScopeMock, { options: { blurOnEnter: true } }), function(scope, element) {
+      spyOn(element[0], 'blur');
+      var event = $.Event('keypress');
+      event.which = 13;
+      element.triggerHandler(event);
+      expect(element[0].blur).toHaveBeenCalled();
+    });
   });
 });
+
