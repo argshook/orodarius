4e0fef6f4f8790f6d4e6c6ef0231faa524d9b9d4
argshook <argshook@gmail.com>
2016-03-28 16:38:06 +0300

    sidebar-empty: refactor into component

--- app/js/sidebar/orodarius-sidebar-empty.directive.js
+++ app/js/sidebar/orodarius-sidebar-empty.directive.js
@@ -1,32 +1,26 @@
-;(function () {
+;(() => {
   'use strict';
 
   angular
     .module('orodarius')
-    .directive('orodariusSidebarEmpty', function() {
-      return {
-        restrict: 'E',
-        scope: {
-          onSubredditClick: '&'
-        },
-        templateUrl: 'views/sidebar/sidebar-empty.html',
-        bindToController: true,
-        controllerAs: '$ctrl',
-        controller: function(LastSubredditsService) {
-          this.lastSubreddits = LastSubredditsService.getList();
+    .component('orodariusSidebarEmpty', {
+      bindings: {
+        onSubredditClick: '&'
+      },
+      templateUrl: 'views/sidebar/sidebar-empty.html',
+      controller: function(LastSubredditsService) {
+        this.lastSubreddits = LastSubredditsService.getList();
 
-          this.suggestedSubreddits = [
-            { name: 'videos' },
-            { name: 'youtubehaiku' },
-            { name: 'artisanvideos' },
-            { name: 'listentothis' },
-            { name: 'gamephysics' },
-            { name: 'music' },
-            { name: 'videos+youtubehaiku' }
-          ];
-        }
-      };
+        this.suggestedSubreddits = [
+          { name: 'videos' },
+          { name: 'youtubehaiku' },
+          { name: 'artisanvideos' },
+          { name: 'listentothis' },
+          { name: 'gamephysics' },
+          { name: 'music' },
+          { name: 'videos+youtubehaiku' }
+        ];
+      }
     });
-}());
-
+})();
 
