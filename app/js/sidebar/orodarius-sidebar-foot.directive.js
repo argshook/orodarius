fb2ac8aa98c2663740cfcda5715125c08ce3401f
argshook <argshook@gmail.com>
2016-03-28 16:55:08 +0300

    sidebar-foot: refactor to component

--- app/js/sidebar/orodarius-sidebar-foot.directive.js
+++ app/js/sidebar/orodarius-sidebar-foot.directive.js
@@ -1,29 +1,25 @@
-;(function () {
+;(() => {
   'use strict';
 
   angular
     .module('orodarius')
-    .directive('orodariusSidebarFoot', function() {
-      return {
-        restrict: 'E',
-        scope: {},
-        templateUrl: 'views/sidebar/sidebar-foot.html',
-        controllerAs: '$ctrl',
-        controller: function($http) {
-          this.lastUpdatedData = {};
+    .component('orodariusSidebarFoot', {
+      bindings: {},
+      templateUrl: 'views/sidebar/sidebar-foot.html',
+      controller: function($http) {
+        this.lastUpdatedData = {};
 
-          this.getLastUpdated = function() {
-            $http.get('https://api.github.com/repos/argshook/orodarius/commits/gh-pages')
-            .then(data => this.lastUpdatedData = {
-              url: data.data.html_url,
-              date: data.data.commit.author.date,
-              message: data.data.commit.message
-            });
-          };
+        this.getLastUpdated = function() {
+          $http.get('https://api.github.com/repos/argshook/orodarius/commits/gh-pages')
+          .then(data => this.lastUpdatedData = {
+            url: data.data.html_url,
+            date: data.data.commit.author.date,
+            message: data.data.commit.message
+          });
+        };
 
-          this.getLastUpdated();
-        }
-      };
+        this.getLastUpdated();
+      }
     });
-}());
+})();
 
