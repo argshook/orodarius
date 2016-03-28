a904f1bcb8a001928413a39aaa50c4c5e79e4044
argshook <argshook@gmail.com>
2016-03-28 22:51:30 +0300

    app.js: reformat

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,24 @@
-;(function() {
-  angular.module('orodarius', ['LocalStorageModule', 'ngAnimate'])
-  .config(function(localStorageServiceProvider) {
-    localStorageServiceProvider.setPrefix('orodarius');
-  })
-  .run(function($window) {
-    $($window).on('resize', _.throttle(function() {
-      $('#main-video-player')
-        .attr('width', $window.innerWidth)
-        .attr('height', $window.innerHeight);
-    }, 400));
-  });
+;(() => {
+  angular
+    .module('orodarius', [
+      'LocalStorageModule',
+      'ngAnimate'
+    ]);
+
+  angular
+    .module('orodarius')
+    .config(function(localStorageServiceProvider) {
+      localStorageServiceProvider.setPrefix('orodarius');
+    })
+
+  angular
+    .module('orodarius')
+    .run(function($window) {
+      $($window).on('resize', _.throttle(function() {
+        $('#main-video-player')
+          .attr('width', $window.innerWidth)
+          .attr('height', $window.innerHeight);
+      }, 400));
+    });
 })();
+
