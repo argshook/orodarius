b25201f0ffc8405ed93177a404e233d6062e743b
argshook <argshook@gmail.com>
2016-04-03 18:02:50 +0300

    SidebarSerice: add state manager

--- app/js/services/sidebar-service.js
+++ app/js/services/sidebar-service.js
@@ -3,6 +3,38 @@
 
   angular.module('orodarius')
     .service('SidebarService', function() {
+      // TODO extract this to factory
+      this.state = new function StateManager() {
+        let CURRENT_STATE = '';
+        let LISTENERS = [];
+
+        const set = state =>
+          CURRENT_STATE = state;
+
+        const get = () =>
+          CURRENT_STATE;
+
+        const invokeListeners = () =>
+          _.each(LISTENERS, listener => listener());
+
+        const registerListener = listener => {
+          LISTENERS = [listener].concat(LISTENERS);
+          return listener;
+        }
+
+        const deregisterListener = listener =>
+          LISTENERS = _.without(LISTENERS, listener);
+
+        const createDeregisterer = listener =>
+          () => deregisterListener(listener)
+
+        return {
+          get,
+          set: _.flow(set, invokeListeners),
+          subscribe: _.flow(registerListener, createDeregisterer)
+        };
+      };
+
       this.isOpen = true;
 
       this.toggle = function(toggleValue) {
--- test/unit/services/sidebar-service.spec.js
+++ test/unit/services/sidebar-service.spec.js
@@ -1,35 +1,72 @@
 'use strict';
 
 describe("Service: SidebarService", function() {
-  var service;
+  var SidebarService;
 
   beforeEach(function() {
     module('orodarius');
-    inject(function (SidebarService) {
-      service = SidebarService;
+    inject(_SidebarService_ => {
+      SidebarService = _SidebarService_;
     });
   });
 
   it('should expose isOpen value set to true initially', function() {
-    expect(service.isOpen).toBe(true);
+    expect(SidebarService.isOpen).toBe(true);
   });
 
   describe('toggle()', function() {
     it('should toggle isOpen value', function() {
-      service.toggle();
-      expect(service.isOpen).toBe(false);
+      SidebarService.toggle();
+      expect(SidebarService.isOpen).toBe(false);
 
-      service.toggle();
-      expect(service.isOpen).toBe(true);
+      SidebarService.toggle();
+      expect(SidebarService.isOpen).toBe(true);
     });
   });
 
   describe('close()', function() {
     it('should set isOpen value to false', function() {
-      service.isOpen = true;
-      service.close();
-      expect(service.isOpen).toBe(false);
+      SidebarService.isOpen = true;
+      SidebarService.close();
+      expect(SidebarService.isOpen).toBe(false);
     });
   });
 
+  describe('state', () => {
+    it('should be an object with expected methods', () => {
+      expect(SidebarService.state).toEqual(jasmine.objectContaining({
+        get: jasmine.any(Function),
+        set: jasmine.any(Function),
+        subscribe: jasmine.any(Function)
+      }));
+    });
+
+    describe('set() & get()', function() {
+      it('should set and return state.current', function() {
+        SidebarService.state.set('anotherState');
+        expect(SidebarService.state.get()).toBe('anotherState');
+      });
+    });
+
+    describe('subscribe()', function() {
+      it('should register listener to be called on set()', function() {
+        let listenerSpy = jasmine.createSpy('listenerSpy');
+        SidebarService.state.subscribe(listenerSpy);
+        SidebarService.state.set('something');
+        expect(listenerSpy).toHaveBeenCalled();
+      });
+
+      it('should return listener deregisterer', function() {
+        let listenerSpy = jasmine.createSpy('listenerSpy');
+        let deregisterer = SidebarService.state.subscribe(listenerSpy);
+
+        SidebarService.state.set('something');
+        expect(listenerSpy.calls.count()).toBe(1);
+        deregisterer();
+        SidebarService.state.set('something else');
+        expect(listenerSpy.calls.count()).toBe(1);
+      });
+    });
+  });
 });
+
